63bd2d57c79458b75645536e2c82b036dfa151b4
Joltra <MinnyMouseXDmail.com>
2022-08-13 21:10:14 +0200

    Add constant class

---
+++ src/main/java/net/theevilreaper/apis/api/Constants.java
@@ -0,0 +1,14 @@
+package net.theevilreaper.apis.api;
+
+public class Constants {
+
+    public static final String ROOM_ID = "_Room__id";
+
+    public static final String ROOM_X = "_Room__x";
+
+    public static final String ROOM_Y = "_Room__y";
+
+    public static final String ROOM_DOORS = "doors";
+
+    public static final String ROOM_TYPE = "room_type";
+}
