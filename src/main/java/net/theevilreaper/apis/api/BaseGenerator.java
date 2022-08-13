267c72c16b8ddd532ebbb3cf1fbab243b80caaf5
Joltra <MinnyMouseXDmail.com>
2022-08-13 21:11:36 +0200

    Add logic to the loadData method

--- src/main/java/net/theevilreaper/apis/api/BaseGenerator.java
+++ src/main/java/net/theevilreaper/apis/api/BaseGenerator.java
@@ -1,29 +1,38 @@
 package net.theevilreaper.apis.api;
 
-import net.minestom.server.coordinate.Point;
-import net.minestom.server.entity.Player;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.minestom.server.instance.Instance;
+import net.theevilreaper.apis.api.data.DoorFace;
+import net.theevilreaper.apis.api.data.LoadedRoom;
+import net.theevilreaper.apis.api.data.RoomType;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+import static net.theevilreaper.apis.api.Constants.*;
+
 public abstract class BaseGenerator implements DungeonGenerator {
 
-    protected static Logger LOGGER = LoggerFactory.getLogger(BaseGenerator.class);
+    protected static Logger generatorLogger = LoggerFactory.getLogger(BaseGenerator.class);
 
     protected final Instance instance;
 
     protected final Path filePath;
 
-    protected Object[][] floorPlan;
+    protected LoadedRoom[][] floorPlan;
 
-    protected Object startRoom;
+    protected LoadedRoom startRoom;
 
-    protected List<Object> loadedRooms;
+    protected List<LoadedRoom> loadedRooms;
 
     private final String name;
 
@@ -34,21 +43,80 @@ public abstract class BaseGenerator implements DungeonGenerator {
         this.loadedRooms = new ArrayList<>();
     }
 
-    public abstract void loadData();
+    public void loadData() {
+        if (!Files.exists(filePath)) {
+            throw new NullPointerException("The given path does not exist");
+        }
 
-    @Override
-    public void generate(@NotNull Point startPos) {
+        try {
+            JsonObject entry = GSON.fromJson(new InputStreamReader(Files.newInputStream(filePath), StandardCharsets.UTF_8), JsonObject.class);
 
-    }
+            if (!entry.has("height")) {
+                throw new NullPointerException("The height attribute is missing");
+            }
 
-    @Override
-    public void generate(@NotNull Player player) {
-        this.generate(player.getPosition().asVec());
+            var height = entry.get("height").getAsInt();
+
+            if (!entry.has("width")) {
+                throw new NullPointerException("The width attribute is missing");
+            }
+
+            var width = entry.get("width").getAsInt();
+
+            this.floorPlan = new LoadedRoom[height][width];
+
+            if (!entry.has("floor")) {
+                throw new NullPointerException("The floor attribute is missing");
+            }
+
+            var floor = entry.get("floor").getAsJsonArray();
+
+            if (floor.isEmpty()) {
+                throw new NullPointerException("The floor can not be empty");
+            }
+
+            for (JsonElement jsonElement : floor) {
+                JsonObject asJsonObject = jsonElement.getAsJsonObject();
+
+                var x = asJsonObject.get(ROOM_X).getAsInt();
+                var y = asJsonObject.get(ROOM_Y).getAsInt();
+
+                var roomType = RoomType.getRoomType(asJsonObject.get(ROOM_TYPE).getAsInt());
+
+                var doorArray = asJsonObject.get(ROOM_DOORS).getAsJsonArray();
+
+                var doors = new DoorFace[doorArray.size()];
+
+                int counter = 0;
+
+                for (JsonElement element : doorArray) {
+                    doors[counter] = DoorFace.getFace( element.getAsInt());
+                    counter++;
+                }
+
+                var room = new LoadedRoom(x ,y, roomType, doors);
+
+                if (startRoom == null) {
+                    startRoom = room;
+                }
+
+                this.floorPlan[y][x] = room;
+                this.loadedRooms.add(room);
+            }
+        } catch (IOException exception) {
+
+        }
     }
 
     @NotNull
     @Override
     public String getName() {
         return name;
+    }
+
+    @NotNull
+    @Override
+    public LoadedRoom[][] getFloorPlan() {
+        return floorPlan;
     }
 }
