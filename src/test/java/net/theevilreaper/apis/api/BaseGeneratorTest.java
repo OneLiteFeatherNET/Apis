53d724c17834f06b813fbe3a7e584bd053383020
theEvilReaper <steffenwx@gmail.com>
2022-08-15 11:21:52 +0200

    Add test class for the base generator

---
+++ src/test/java/net/theevilreaper/apis/api/BaseGeneratorTest.java
@@ -0,0 +1,72 @@
+package net.theevilreaper.apis.api;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class BaseGeneratorTest {
+
+    @Order(1)
+    @Test
+    void testFailWithoutPath() {
+        var generator = new DungeonGeneratorImpl("Isaac", null, Paths.get("bla"));
+        var exception = assertThrows(NullPointerException.class, generator::loadData);
+        assertSame(NullPointerException.class, exception.getClass());
+        assertEquals("The given path does not exist", exception.getMessage());
+    }
+
+    @Order(2)
+    @Test
+    void testMissingHeightValue() {
+        Path resourceDirectory = Paths.get("src","test","resources");
+        var filePath = resourceDirectory.resolve("dungeon_failed_all.json");
+
+        var generator = new DungeonGeneratorImpl("Isaac", null, filePath);
+
+        var exception = assertThrows(NullPointerException.class, generator::loadData);
+        assertEquals("The height attribute is missing", exception.getMessage());
+    }
+
+    @Order(3)
+    @Test
+    void testMissingWidthValue() {
+        Path resourceDirectory = Paths.get("src","test","resources");
+        var filePath = resourceDirectory.resolve("dungeon_miss_width.json");
+
+        var generator = new DungeonGeneratorImpl("Isaac", null, filePath);
+
+        var exception = assertThrows(NullPointerException.class, generator::loadData);
+        assertEquals("The width attribute is missing", exception.getMessage());
+    }
+
+    @Order(4)
+    @Test
+    void testMissingFloorValue() {
+        Path resourceDirectory = Paths.get("src","test","resources");
+        var filePath = resourceDirectory.resolve("dungeon_miss_height.json");
+
+        var generator = new DungeonGeneratorImpl("Isaac", null, filePath);
+
+        var exception = assertThrows(NullPointerException.class, generator::loadData);
+        assertEquals("The floor attribute is missing", exception.getMessage());
+    }
+
+    @Order(5)
+    @Test
+    void testMissingDoorValue() {
+        Path resourceDirectory = Paths.get("src","test","resources");
+        var filePath = resourceDirectory.resolve("dungeon_miss_room.json");
+
+        var generator = new DungeonGeneratorImpl("Isaac", null, filePath);
+
+        var exception = assertThrows(NullPointerException.class, generator::loadData);
+        assertEquals("A room must have at least one door", exception.getMessage());
+    }
+}
