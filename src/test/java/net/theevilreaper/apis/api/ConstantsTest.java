e0483d7ad21275a7495063d4fae609b97537f25e
theEvilReaper <steffenwx@gmail.com>
2022-08-15 11:21:33 +0200

    Add test class for constant class

---
+++ src/test/java/net/theevilreaper/apis/api/ConstantsTest.java
@@ -0,0 +1,35 @@
+package net.theevilreaper.apis.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstantsTest {
+
+    @Test
+    void testRoomIDConstant() {
+        assertSame("_Room__id", Constants.ROOM_ID);
+    }
+
+    @Test
+    void testRoomXConstant() {
+        assertSame("_Room__x", Constants.ROOM_X);
+    }
+
+    @Test
+    void testRoomYConstant() {
+        assertSame("_Room__y", Constants.ROOM_Y);
+    }
+
+    @Test
+    void testRoomDoorConstant() {
+        assertSame("doors", Constants.ROOM_DOORS);
+    }
+
+    @Test
+    void testRoomTypeConstant() {
+        assertSame("room_type", Constants.ROOM_TYPE);
+    }
+
+
+}
