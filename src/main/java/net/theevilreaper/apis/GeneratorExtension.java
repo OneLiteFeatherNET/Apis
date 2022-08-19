1ba19009ba7e9babfc8f823705d56f00a9ad5fa2
theEvilReaper <steffenwx@gmail.com>
2022-08-19 22:42:30 +0200

    Add folder check

--- src/main/java/net/theevilreaper/apis/GeneratorExtension.java
+++ src/main/java/net/theevilreaper/apis/GeneratorExtension.java
@@ -1,16 +1,56 @@
 package net.theevilreaper.apis;
 
 import net.minestom.server.extensions.Extension;
+import net.theevilreaper.apis.schematic.LegacyLookup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class GeneratorExtension extends Extension {
 
+    private static final String LEGACY_FILE = "legacy.json";
+
+    private final Logger extensionLogger;
+
+    public GeneratorExtension() {
+        this.extensionLogger = LoggerFactory.getLogger(GeneratorExtension.class);
+    }
+
     @Override
     public void initialize() {
-        // Nothing to do here
+        this.initFolder();
+        new LegacyLookup(getDataDirectory().resolve(LEGACY_FILE));
     }
 
     @Override
     public void terminate() {
         // Nothing to do here
+    }
+
+    private void initFolder() {
+        if (!Files.exists(getDataDirectory())) {
+            try {
+                Files.createDirectories(getDataDirectory());
+            } catch (IOException exception) {
+                extensionLogger.warn("Unable to create data directory of the extension", exception);
+            }
+        }
+
+        var legacyFilePath = getDataDirectory().resolve(LEGACY_FILE);
+
+        if (!Files.exists(legacyFilePath)) {
+            try {
+                var resourceFile = getResource(LEGACY_FILE);
+                if (resourceFile == null) {
+                    return;
+                }
+                Files.copy(resourceFile, legacyFilePath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException exception) {
+                extensionLogger.warn("Unable to copy {} from the resource folder {}", LEGACY_FILE, exception);
+            }
+        }
     }
 }
