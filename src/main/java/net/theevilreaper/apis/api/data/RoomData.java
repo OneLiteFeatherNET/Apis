ccff806bbb767145bbcb6c7b3028587d5e1b119d
theEvilReaper <steffenwx@gmail.com>
2022-08-19 22:42:13 +0200

    Add equals and hashcode method

--- src/main/java/net/theevilreaper/apis/api/data/RoomData.java
+++ src/main/java/net/theevilreaper/apis/api/data/RoomData.java
@@ -1,12 +1,42 @@
 package net.theevilreaper.apis.api.data;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * @author Joltra
  * @version 1.0.0
  * @since 1.0.0
  **/
-//TODO: Rework the class
 public record RoomData(int x, int z, RoomType type, DoorFace... doors) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoomData roomData = (RoomData) o;
+        return x == roomData.x && z == roomData.z && type == roomData.type && Arrays.equals(doors, roomData.doors);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(x, z, type);
+        result = 31 * result + Arrays.hashCode(doors);
+        return result;
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+        return "LoadedRoom{" +
+                "x=" + x +
+                ", z=" + z +
+                ", type=" + type +
+                '}';
+    }
 
     public boolean isStart() {
         return this.type == RoomType.START_ROOM;
@@ -18,15 +48,6 @@ public record RoomData(int x, int z, RoomType type, DoorFace... doors) {
 
     public boolean isBoss() {
         return this.type == RoomType.BOSS_ROOM;
-    }
-
-    @Override
-    public String toString() {
-        return "LoadedRoom{" +
-                "x=" + x +
-                ", z=" + z +
-                ", type=" + type +
-                '}';
     }
 
     public boolean isItem() {
