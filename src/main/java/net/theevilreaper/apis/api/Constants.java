67784d99c5f7d4428270939fd8672ba3df7a0a3b
theEvilReaper <steffenwx@gmail.com>
2022-09-04 22:57:25 +0200

    Add new constant values

--- src/main/java/net/theevilreaper/apis/api/Constants.java
+++ src/main/java/net/theevilreaper/apis/api/Constants.java
@@ -10,12 +10,10 @@ public final class Constants {
     private Constants() {}
 
     public static final String ROOM_ID = "_Room__id";
-
     public static final String ROOM_X = "_Room__x";
-
     public static final String ROOM_Y = "_Room__y";
-
     public static final String ROOM_DOORS = "doors";
-
     public static final String ROOM_TYPE = "room_type";
+    public static final String REGION_FILE = ".json";
+    public static final String SCHEMATIC_FILE = ".schem";
 }
