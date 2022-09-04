9a0c96a7247529afc6ac340b4fb99c25679f7db6
theEvilReaper <steffenwx@gmail.com>
2022-09-04 21:05:16 +0200

    Add documentation

--- src/main/java/net/theevilreaper/apis/api/data/DoorFace.java
+++ src/main/java/net/theevilreaper/apis/api/data/DoorFace.java
@@ -1,6 +1,9 @@
 package net.theevilreaper.apis.api.data;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
+ * The enum includes all possible directions where a room can have doors.
  * @author Joltra
  * @version 1.0.0
  * @since 1.0.0
@@ -12,24 +15,41 @@ public enum DoorFace {
     LEFT(3, "LEFT");
 
     private final int id;
-
     private final String name;
-
     private static final DoorFace[] VALUES = values();
 
+    /**
+     * Creates a reference from a DoorFace.
+     * @param id the id for the door
+     * @param name the name for the door
+     */
     DoorFace(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * Returns the id from the door.
+     * @return the given id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the name from a door entry.
+     * @return the give name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns an enum entry from a door by his id.
+     * @param id the id from the door
+     * @return the fetched entry or null
+     */
+    @Nullable
     public static DoorFace getFace(int id) {
         return VALUES[id];
     }
