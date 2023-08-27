714c8b2e1e65bc4098bbba7bc5e1d54f26bae05c
theEvilReaper <steffenwx@gmail.com>
2023-08-27 22:44:32 +0200

    Enhance RoomUnit structure

---
+++ src/main/java/net/theevilreaper/apis/api/generator/unit/RoomUnitBuilder.java
@@ -0,0 +1,49 @@
+package net.theevilreaper.apis.api.generator.unit;
+
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.instance.Chunk;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since
+ **/
+
+public non-sealed class RoomUnitBuilder implements RoomUnit.Builder {
+
+    private Path schematicPath;
+    private Vec originPoint;
+    private final Map<Vec, Chunk> chunkMap;
+
+    public RoomUnitBuilder() {
+        this.chunkMap = new HashMap<>();
+    }
+
+    @Override
+    public RoomUnit.@NotNull Builder setSchematicPath(@NotNull Path path) {
+        this.schematicPath = path;
+        return this;
+    }
+
+    @Override
+    public RoomUnit.@NotNull Builder setOriginPoint(@NotNull Vec vec) {
+        this.originPoint = vec;
+        return this;
+    }
+
+    @Override
+    public RoomUnit.@NotNull Builder addChunk(@NotNull Vec vec, @NotNull Chunk chunk) {
+        this.chunkMap.put(vec, chunk);
+        return this;
+    }
+
+    @Override
+    public @NotNull RoomUnit build() {
+        return new RoomUnitImpl(this.schematicPath, this.originPoint, this.chunkMap);
+    }
+}
