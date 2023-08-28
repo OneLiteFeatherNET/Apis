c677ff39655e51f94194dd67e6019f1647595aa4
theEvilReaper <steffenwx@gmail.com>
2023-08-28 22:06:31 +0200

    Add documentation

--- src/main/java/net/theevilreaper/apis/api/generator/functional/OriginPointPartCalculation.java
+++ src/main/java/net/theevilreaper/apis/api/generator/functional/OriginPointPartCalculation.java
@@ -1,13 +1,23 @@
 package net.theevilreaper.apis.api.generator.functional;
 
 /**
+ * The interface can be implemented to define the calculation process for the positions which are needed to place
+ * a room correctly into the world.
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
 @FunctionalInterface
 public interface OriginPointPartCalculation {
 
-   int calculatePointPart(int startPoint, int currentPoint, int roomScale);
+   /**
+    * Returns an integer which is used to determine the right room position.
+    * @param oldStartRoom the old start coordinate
+    * @param startPoint the position coordinate from the startRoom
+    * @param currentPoint the current position
+    * @param roomScale the scale of a room
+    * @return the calculated part of a position
+    */
+   int calculatePointPart(int oldStartRoom, int startPoint, int currentPoint, int roomScale);
 
 }
--- src/main/java/net/theevilreaper/apis/api/generator/functional/SchematicPlacement.java
+++ src/main/java/net/theevilreaper/apis/api/generator/functional/SchematicPlacement.java
@@ -1,6 +1,6 @@
 package net.theevilreaper.apis.api.generator.functional;
 
-import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -8,12 +8,30 @@ import org.jetbrains.annotations.Nullable;
 import java.nio.file.Path;
 
 /**
+ * The interface can be used to bind the logic to place a schematic file into a {@link Instance} from the server.
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
 @FunctionalInterface
 public interface SchematicPlacement {
 
-    void place(@NotNull Instance instance, @NotNull Pos position, @NotNull Path schematicPath, @Nullable Runnable callback);
+    /**
+     * The method contains the logic to place a schematic onto a given {@link Instance}.
+     * @param instance the instance to place the schematic into it
+     * @param position the origin point to place the schematic
+     * @param schematicPath the path to the schematic file
+     */
+    default void place(@NotNull Instance instance, @NotNull Point position, @NotNull Path schematicPath) {
+        this.place(instance, position, schematicPath, null);
+    }
+
+    /**
+     * The method contains the logic to place a schematic onto a given {@link Instance}.
+     * @param instance the instance to place the schematic into it
+     * @param position the origin point to place the schematic
+     * @param schematicPath the path to the schematic file
+     * @param callback a callback which can use triggered to execute after the placement
+     */
+    void place(@NotNull Instance instance, @NotNull Point position, @NotNull Path schematicPath, @Nullable Runnable callback);
 }
