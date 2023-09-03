697e4a6b1e3310d2db31b450e9beefdb97d515e5
theEvilReaper <steffenwx@gmail.com>
2023-09-03 17:06:26 +0200

    Add documentation

--- src/main/java/net/theevilreaper/apis/api/generator/SchematicPlacementTask.java
+++ src/main/java/net/theevilreaper/apis/api/generator/SchematicPlacementTask.java
@@ -17,23 +17,28 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * The task is used to place each schematic into the world to construct the layout.
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
  **/
 @ApiStatus.Internal
 public final class SchematicPlacementTask implements Runnable {
-
-    private final Logger logger;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchematicPlacementTask.class);
     private final Instance instance;
     private final Queue<RoomUnit> roomQueue;
     private final SchematicPlacement schematicPlacement;
     private final Runnable callback;
     private Task task;
 
+    /**
+     * Creates a new reference from this task with given values.
+     * @param instance the instance to place the schematic
+     * @param units a {@link List} which contains {@link RoomUnit} objects for the placement
+     * @param schematicPlacement an implementation of {@link SchematicPlacement} interface
+     * @param callback additional code which should be executed at the end
+     */
     public SchematicPlacementTask(@NotNull Instance instance, @NotNull List<RoomUnit> units, @NotNull SchematicPlacement schematicPlacement, @Nullable Runnable callback) {
-        this.logger = LoggerFactory.getLogger(SchematicPlacementTask.class);
-        this.logger.info("CALLL");
         this.instance = instance;
         this.roomQueue = new ArrayDeque<>(units);
         this.schematicPlacement = schematicPlacement;
@@ -41,12 +46,15 @@ public final class SchematicPlacementTask implements Runnable {
         this.callback = callback;
     }
 
+    /**
+     * The method handles the placement of each room.
+     */
     @Override
     public void run() {
         if (this.roomQueue.isEmpty()) {
             this.task.cancel();
             this.task = null;
-            logger.info("Finished placement of all rooms");
+            LOGGER.info("Finished placement of all rooms");
             if (this.callback != null) {
                this.callback.run();
             }
