530fb9ed7c07c9e419eaf39ce15c349399a3eb88
theEvilReaper <steffenwx@gmail.com>
2024-12-28 12:25:01 +0100

    Update gradle script usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,7 @@ dependencyResolutionManagement {
             }
         } else {
             repositories {
+                mavenCentral()
                 maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
                 maven {
                     val groupdId = 28 // Gitlab Group
@@ -42,24 +43,24 @@ dependencyResolutionManagement {
                         create<HttpHeaderAuthentication>("header")
                     }
                 }
-                maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-                mavenCentral()
-                maven("https://jitpack.io")
+
             }
         }
     }
     versionCatalogs {
         create("libs") {
             version("publishdata", "1.4.0")
-            version("shadow", "8.3.0")
-            library("dungeon.base.bom", "net.theevilreaper.dungeon.bom", "base").version("1.0.6")
+            version("schem", "1.3.1")
+
+            library("dungeon.base.bom", "net.theevilreaper.dungeon.bom", "base").version("1.1.1")
             library("minestom", "net.onelitefeather.microtus", "Microtus").withoutVersion()
             library("minestom-test", "net.onelitefeather.microtus.testing", "testing").withoutVersion()
             library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").withoutVersion()
             library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
             library("mockito-core", "org.mockito", "mockito-core").withoutVersion()
             library("mockito-junit", "org.mockito", "mockito-junit-jupiter").withoutVersion()
-            library("schem", "com.github.hollow-cube.common", "schem").version("db5eab9512")
+            library("schem", "dev.hollowcube", "schem").versionRef("schem")
+
             plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
         }
     }
